10f35b64bd4266dab65186a8f1020d61392576fd
Michael Freeman <michael.k.freeman@gmail.com>
2015-07-21 19:43:00 -0700

    Improved landing page

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ var myApp = angular.module('myApp', ['ngRoute'])
       templateUrl: 'templates/landing.html',
       controller: 'LandingController',
     })
-   .when('/content/', {
-    templateUrl: 'templates/content.html',
-    controller: 'ContentController',
+   .when('/about/', {
+    templateUrl: 'templates/about.html',
+    controller: 'AboutController',
   })
    .when('/teaching/', {
     templateUrl: 'templates/teaching.html',
