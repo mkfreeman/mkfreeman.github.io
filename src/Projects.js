d9c9846050e0348586d5113f86ccc5f8e72eb608
Michael Freeman <michael.k.freeman@gmail.com>
2018-01-18 12:00:32 -0800

    Add alt tags to images

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react';
-import {GridList, GridTile} from 'material-ui/GridList';
+import React, { Component } from 'react';
+import { GridList, GridTile } from 'material-ui/GridList';
 import './Projects.css';
-import ProjectCard from './ProjectCard';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as d3 from 'd3';
 
@@ -15,8 +14,10 @@ class Projects extends Component {
     }
     componentWillMount() {
         d3
-            .csv('./data/projects.csv', function (error, data) {
-                this.setState({data: data});
+            .csv('./data/projects.csv', function(error, data) {
+                this.setState({
+                    data: data
+                });
             }.bind(this))
     }
     render() {
@@ -33,39 +34,22 @@ class Projects extends Component {
         };
         return (
             <div className="Projects">
-                <MuiThemeProvider>
-                    <GridList cols={3} cellHeight={200} padding={1} style={styles.gridList}>
-                        {this
-                            .state
-                            .data
-                            .map((tile) => (
-                                <GridTile
-                                    className="grid-tile"
-                                    subtitleStyle={{
-                                    position: 'absolute',
-                                    'white-space': 'unset',
-                                    'margin-top': '9px'
-                                }}
-                                    key={tile.img}
-                                    title={< a style = {{color:'white', 'fontSize':'1.5em', 'text-decoration':'none'}}href = {
-                                    tile.url
-                                }
-                                target = "_blank" > {
-                                    tile.title
-                                } < /a>}
-                                    subtitle={tile.description}
-                                    actionPosition="left"
-                                    titlePosition="top"
-                                    titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-                                    cols={window.innerWidth < 600
-                                    ? 3
-                                    : tile.cols}
-                                    rows={tile.rows}>
-                                    <img src={tile.img}/>
-                                </GridTile>
-                            ))}
-                    </GridList>
-                </MuiThemeProvider>
+              <MuiThemeProvider>
+                <GridList cols={ 3 } cellHeight={ 200 } padding={ 1 } style={ styles.gridList }>
+                  { this
+                        .state
+                        .data
+                        .map((tile) => (
+                            <GridTile className="grid-tile" subtitleStyle={ { position: 'absolute', 'white-space': 'unset', 'margin-top': '9px' } } key={ tile.img } title={ < a style={ { color: 'white', 'fontSize': '1.5em', 'text-decoration': 'none' } } href={ tile.url } target="_blank">
+                                                                                                                                                                       { tile.title } </a> } subtitle={ tile.description } actionPosition="left"
+                              titlePosition="top" titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)" cols={ window.innerWidth < 600
+                                                                                                                                                                 ? 3
+                                                                                                                                                                 : tile.cols } rows={ tile.rows }>
+                              <img src={ tile.img } alt={ tile.alt } />
+                            </GridTile>
+                        )) }
+                </GridList>
+              </MuiThemeProvider>
             </div>
         )
     }
--- src/ResourceCard.js
+++ src/ResourceCard.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Card, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import React, { Component } from 'react';
+import { Card, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 
 // Card for learning resources
 class ResourceCard extends Component {
@@ -10,20 +10,16 @@ class ResourceCard extends Component {
         console.log('props title ', this.props.data)
         return (
             <div className="cardWrapper">
-                <Card>
-                    <a target="_blank" href={this.props.data.url}>
-                        <CardMedia
-                            className="cardMedia"
-                            overlay={< CardTitle title = {
-                            this.props.data.title
-                        } />}>
-                            <img src={this.props.data.img} alt=""/>
-                        </CardMedia>
-                    </a>
-                    <CardText>
-                        {this.props.data.description}
-                    </CardText>
-                </Card>
+              <Card>
+                <a target="_blank" href={ this.props.data.url }>
+                  <CardMedia className="cardMedia" overlay={ < CardTitle title={ this.props.data.title } /> }>
+                    <img src={ this.props.data.img } alt={ this.props.data.alt } />
+                  </CardMedia>
+                </a>
+                <CardText>
+                  { this.props.data.description }
+                </CardText>
+              </Card>
             </div>
         )
     }
