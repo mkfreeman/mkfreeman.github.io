ffc0134dfbaf8bdf01845b432b2f13aaa1c27362
mkfreeman <michael.k.freeman@gmail.com>
2020-05-26 10:44:43 -0400

    Change title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Resources from './Resources';
 import Projects from './Projects';
 import About from './About';
@@ -23,7 +23,7 @@ class App extends Component {
             <div className="App">
                 <div className="App-header">
                     <h1>Michael Freeman</h1>
-                    <h2>Senior Lecturer at&nbsp;
+                    <h2>Teaching Professor at&nbsp;
                         <a href="https://ischool.uw.edu" target="_blank">UW iSchool</a>
                     </h2>
                     <h2>Data Science. Visualization. Web Development.</h2>
@@ -53,30 +53,42 @@ class App extends Component {
                             <a href="./freeman-cv.pdf">vita</a>
                         </span>
                     </h3>
-                    <Isvg className="burst" src="imgs/sunburst.svg" />
+                    <Isvg className="burst" src="imgs/sunburst.svg"/>
                 </div>
                 <section id="resources">
                     <h1 className="section-title">Learning Resources</h1>
                     <p className="section-description">I develop open-source learning resources to
                         improve access to and understanding of programming languages.</p>
-                    <Resources />
+                    <Resources/>
                 </section>
                 <section id="book">
                     <h1 className="section-title">Book</h1>
-                    <p className="section-description">Check out my new book, <em>Programming Skills for Data Science</em>! You can get it <a rel="noopener noreferrer" target="_blank" href="https://www.amazon.com/gp/product/0135133106">online</a>, or learn more about it <a rel="noopener noreferrer" target="_blank" href="https://programming-for-data-science.github.io/">here</a>.</p>
+                    <p className="section-description">Check out my new book,
+                        <em>Programming Skills for Data Science</em>! You can get it
+                        <a
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            href="https://www.amazon.com/gp/product/0135133106">online</a>, or learn more about it
+                        <a
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            href="https://programming-for-data-science.github.io/">here</a>.</p>
                     {/* <div id="book-cover" /> */}
-                    <a rel="noopener noreferrer" target="_blank" href="https://www.amazon.com/gp/product/0135133106"><img src="/imgs/front-cover.png" id="book-img" /></a>
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://www.amazon.com/gp/product/0135133106"><img src="/imgs/front-cover.png" id="book-img"/></a>
                 </section>
                 <section className="projects" id="projects">
                     <h1 className="section-title">Selected Projects</h1>
                     <p className="section-description">I've worked on a variety of large-scale
                         visualization and data science projects. Here are some of the things I've built.</p>
-                    <Projects />
+                    <Projects/>
                 </section>
                 <section className="about" id="about">
                     <h1 className="section-title">About</h1>
                     <p className="section-description">A (very) little bit about me.</p>
-                    <About />
+                    <About/>
                 </section>
             </div>
         );
